a5114e8b5e5d95fef986e4ad675243f43a29dce8
itigan <tigan.ion@gmail.com>
2020-04-28 13:32:06 +0300

    init raw rock paper scissors game

--- README.md
+++ README.md
@@ -1,68 +1,34 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Learning React here
 
-## Available Scripts
+PROs
+* really small
+* really good for SPAs (single page apps)
+* 'highly' modular
 
-In the project directory, you can run:
+CONs
+*  bad for SEO? (this is a problem for all SPAs)
 
-### `yarn start`
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+** In react everything is a component
+Every componets needs to have/know 2 things
+1) how to render itself
+2) what other components it needs
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+** Data flows one way, like waterfall
+It should go from top to bottom, from the root to it's children
+! Children are allowed to pass data up to the parents, as in call calbacks and stuff - they
+are not allowed to change the state directly in the parent
 
-### `yarn test`
+** State
+* Stateless and statefull componets
+* the state should be internal to a component
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+** Properties
+ These are passed from the parent to it's children components
+ Should not be stored in the component state
 
-### `yarn build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+** Controllers
+ Have children without state, only props
+ 
+** Single Source of Thruth
+ ?? When the parent owns 
--- src/App.js
+++ src/App.js
@@ -1,26 +1,154 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Rock Paper Sccisors</h1>
+        <Game/>
+      </div>
+      )
+  }
+}
+
+class Game extends React.Component {
+  constructor(props){
+    super(props);
+
+    /* 
+      this.state assignment can be done just in the constructor
+      outside we must use this.setState() as react knows how to tract the changes and then update the components
+    */
+    this.state = {
+      played: false,
+      status: null,
+      playerMove: null,
+      opponentMove: null,
+    };
+
+    /*
+     * this is the binding in action, in order to have the components see/call our funcitons we need to bind them
+     * notice: that I did not bind the method at first, and it kind of worked, well it worked as long as I didn't try to access 'this'
+     * most of the time if the function you are calling fails with 'can't access/call method on 'this' because undefined' the most probably you are missing the binding
+     */
+    this.rock = this.rock.bind(this);
+    this.paper = this.paper.bind(this);
+    this.scissors = this.scissors.bind(this);
+    this.playerMove = this.playerMove.bind(this);
+    this.resetGame = this.resetGame.bind(this);
+  }
+
+  playerMove(playerMove){
+    if(!this.state.played){
+      let opponentMove = chooseRandomAction();
+      let status = chooseWinner(playerMove, opponentMove);
+      
+      // here we update the state
+      this.setState({
+        played: true,
+        status: status,
+        playerMove: playerMove,
+        opponentMove: opponentMove,
+      });
+
+    } else {
+      alert('game finished, must restart');
+    }
+  }
+
+  rock(){
+    this.playerMove('Rock');
+  }
+
+  paper(){
+    this.playerMove('Paper');
+  }
+
+  scissors(){
+    this.playerMove('Scissors');
+  }
+
+  resetGame(){
+    this.setState({
+      played: false,
+      status: null,
+      playerMove: null,
+      opponentMove: null,
+    });
+  }
+
+  render(){
+
+    /**
+     * Here we can execute other code before the actual render
+     */
+    let gameStatus = this.state.played ? translateStatusToHuman(this.state.status) : '';
+    let playerMove = this.state.played ? this.state.playerMove : '';
+    let opponentMove = this.state.played ? this.state.opponentMove : '';
+
+    let resetButton = this.state.played ? <button onClick={this.resetGame}>Reset</button> : '';
+    
+
+    return (
+      <div>
+          <button onClick={this.rock}>Rock</button>
+          <button onClick={this.paper}>Paper</button>
+          <button onClick={this.scissors}>Sccisors</button>
+          <br/>
+          <span> Game status: {gameStatus} </span>
+          <br/>
+          <span> Player move: {playerMove} </span>
+          <br/>
+          <span> CPU choice: {opponentMove} </span>
+          <br/>
+          {resetButton}
+      </div>
+     )
+  }
+}
+
+function chooseRandomAction(){
+  let moves = ['Rock', 'Paper', 'Scissors'];
+  let randomNumber = Math.floor(Math.random() * 3);
+  let randomMove = moves[randomNumber];
+  return randomMove; 
+}
+
+/*
+ -1 player lost
+ 0 tie
+ 1 player won
+*/
+function chooseWinner(playerMove, opponentMove){
+  if (playerMove === opponentMove){
+    return 0;
+  }
+
+  if (playerMove === 'Rock'){
+    if(opponentMove === 'Paper') return -1;
+    if(opponentMove === 'Scissors') return 1;
+  }
+
+  if (playerMove === 'Paper'){
+    if(opponentMove === 'Rock') return 1;
+    if(opponentMove === 'Scissors') return -1;
+  }
+
+  if (playerMove === 'Scissors'){
+    if(opponentMove === 'Paper') return 1;
+    if(opponentMove === 'Rock') return -1;
+  }
+}
+
+// we can use arrow functions as well
+let translateStatusToHuman = (status) => {
+  if(status === -1){
+    return 'Player lost';
+  } else if (status === 1) {
+    return 'Player won';
+  } else {
+    return 'Tie';
+  }
 }
 
 export default App;
